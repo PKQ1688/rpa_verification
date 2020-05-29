6516b7738315e166b15ab56549f3206bef7f5566
PKQ <zhutaonan@i-i.ai>
2020-05-29 17:15:15 +0800

    update

--- verification_service/verification_main.py
+++ verification_service/verification_main.py
@@ -26,11 +26,34 @@ def ocr_pipeline_main(image_with_base64, scenes):
 
 if __name__ == '__main__':
     import base64
+    import random
 
     img_path = '/home/shizai/adolf/data/jindie/EHHf.png'
-    with open(os.path.join(img_path), 'rb') as f:
+    with open(img_path, 'rb') as f:
         image = f.read()
         image_base64 = str(base64.b64encode(image), 'utf-8')
 
     ocr_predict = ocr_pipeline_main(image_base64, 'jindie')
     print(ocr_predict)
+
+    img_p = '/home/shizai/adolf/data/jindie/'
+    img_list = os.listdir(img_p)
+    random.shuffle(img_list)
+    total = 200
+    positive = 0
+    for img_name in img_list[:total]:
+        with open(os.path.join(img_p,img_name), 'rb') as f:
+            image = f.read()
+            image_base64 = str(base64.b64encode(image), 'utf-8')
+
+        ocr_predict = ocr_pipeline_main(image_base64, 'jindie')
+        # print(ocr_predict)
+        # print(img_name)
+        # print('---------')
+        if ocr_predict == img_name.split('.')[0]:
+            positive += 1
+        else:
+            print(img_name)
+            print(ocr_predict)
+
+    print('accuracy', positive / total)
