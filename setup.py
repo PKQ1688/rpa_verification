f13004cb704565b9cafa1cbb2143b4916cff05b3
PKQ <zhutaonan@i-i.ai>
2020-06-17 15:48:25 +0800

    update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rpa_ocr",
-    version="0.0.9",
+    version="0.1.0",
     author="adolf",
     author_email="adolf1321794021@gmail.com",
     description="A Tools for use algorithm for verification recognition",
